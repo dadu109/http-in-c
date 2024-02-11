530d4a2e26c5dd7db31624690c4942be418f10c3
dmiczek <dmiczek@box.com>
2024-02-11 16:24:22 +0100

    feat: define constants

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "server.h"
 #include <stdlib.h>
 
+#define PORT 80
+#define MAX_ACTIVE_CONNECTIONS 1
+
 int main() {
-  start_server(80, 1);
+  start_server(PORT, MAX_ACTIVE_CONNECTIONS);
 
   return EXIT_SUCCESS;
 }
